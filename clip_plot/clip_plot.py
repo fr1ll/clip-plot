e47652b74503104078510436e43481c4c0c0ba1d
Will Sanger <willsa@hey.com>
2023-01-24 14:44:00 -0600

    Add index.ipynb with old version of readme

--- clip_plot/clip_plot.py
+++ clip_plot/clip_plot.py
@@ -6,10 +6,18 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-##
-# Unconditional imports
-##
+# %% auto 0
+__all__ = ['config', 'args', 'timestamp', 'process_images', 'preprocess_kwargs', 'copy_web_assets', 'filter_images',
+           'get_image_paths', 'stream_images', 'clean_filename', 'get_metadata_list', 'write_metadata', 'is_number',
+           'get_manifest', 'get_atlas_data', 'save_atlas', 'get_layouts', 'get_inception_vectors', 'get_umap_layout',
+           'process_single_layout_umap', 'process_multi_layout_umap', 'save_model', 'load_model', 'get_umap_model',
+           'get_rasterfairy_layout', 'get_lap_layout', 'get_alphabetic_layout', 'get_pointgrid_layout',
+           'get_custom_layout', 'get_date_layout', 'datestring_to_date', 'date_to_seconds', 'round_date',
+           'get_categorical_layout', 'get_categorical_boxes', 'get_categorical_points', 'Box', 'get_geographic_layout',
+           'process_geojson', 'get_path', 'write_layout', 'round_floats', 'write_json', 'read_json', 'get_hotspots',
+           'get_cluster_model', 'get_heightmap', 'write_images', 'get_version', 'Image', 'parse']
 
+# %% ../nbs/00_clip_plot.ipynb 4
 from os.path import join, exists, dirname, realpath
 from distutils.dir_util import copy_tree
 from pathlib import Path
@@ -26,11 +34,7 @@ def timestamp():
     """Return a string for printing the current time"""
     return str(datetime.datetime.now()) + ":"
 
-
-##
-# Image processing imports
-##
-
+# %% ../nbs/00_clip_plot.ipynb 6
 if "--copy_web_only" not in sys.argv:
 
     from sklearn.metrics import pairwise_distances_argmin_min
@@ -68,10 +72,9 @@ if "--copy_web_only" not in sys.argv:
     from tensorflow.keras.models import Model
     from tensorflow import compat
 
-
-    ##
-    # Optional install imports
-    ##
+# %% ../nbs/00_clip_plot.ipynb 8
+if "--copy_web_only" not in sys.argv:
+    
     from hdbscan import HDBSCAN
     cluster_method = "hdbscan"
 
@@ -96,18 +99,7 @@ NB: Keras Image class objects return image.size as w,h
 """
 
 
-# %% auto 0
-__all__ = ['config', 'args', 'timestamp', 'process_images', 'preprocess_kwargs', 'filter_images', 'get_image_paths',
-           'stream_images', 'clean_filename', 'get_metadata_list', 'write_metadata', 'is_number', 'get_manifest',
-           'get_atlas_data', 'save_atlas', 'get_layouts', 'get_inception_vectors', 'get_umap_layout',
-           'process_single_layout_umap', 'process_multi_layout_umap', 'save_model', 'load_model', 'get_umap_model',
-           'get_rasterfairy_layout', 'get_lap_layout', 'get_alphabetic_layout', 'get_pointgrid_layout',
-           'get_custom_layout', 'get_date_layout', 'datestring_to_date', 'date_to_seconds', 'round_date',
-           'get_categorical_layout', 'get_categorical_boxes', 'get_categorical_points', 'Box', 'get_geographic_layout',
-           'process_geojson', 'get_path', 'write_layout', 'round_floats', 'write_json', 'read_json', 'get_hotspots',
-           'get_cluster_model', 'get_heightmap', 'write_images', 'get_version', 'Image', 'parse']
-
-# %% ../nbs/00_clip_plot.ipynb 3
+# %% ../nbs/00_clip_plot.ipynb 9
 config = {
     "images": None,
     "metadata": None,
@@ -135,11 +127,7 @@ config = {
     "geojson": None,
 }
 
-# %% ../nbs/00_clip_plot.ipynb 4
-##
-# Entry
-##
-
+# %% ../nbs/00_clip_plot.ipynb 11
 def process_images(**kwargs):
     """Main method for processing user images and metadata"""
     kwargs = preprocess_kwargs(**kwargs)
@@ -162,12 +150,24 @@ def preprocess_kwargs(**kwargs):
             kwargs[i] = [kwargs[i]]
     return kwargs
 
-# %% ../nbs/00_clip_plot.ipynb 7
-##
-# Images
-##
+# %% ../nbs/00_clip_plot.ipynb 12
+def copy_web_assets(**kwargs):
+    """Copy the /web directory from the clipplot source to the users cwd"""
+    src = join(dirname(realpath(__file__)), "web")
+    dest = join(os.getcwd(), kwargs["out_dir"])
+    copy_tree(src, dest)
+    # write version numbers into output
+    for i in ["index.html", os.path.join("assets", "js", "tsne.js")]:
+        path = join(dest, i)
+        with open(path, "r") as f:
+            f = f.read().replace("VERSION_NUMBER", get_version())
+            with open(path, "w") as out:
+                out.write(f)
+    if kwargs["copy_web_only"]:
+        print(timestamp(), "Done!")
+        sys.exit()
 
-
+# %% ../nbs/00_clip_plot.ipynb 15
 def filter_images(**kwargs):
     """Main method for filtering images given user metadata (if provided)"""
     # validate that input image names are unique
@@ -261,7 +261,7 @@ def filter_images(**kwargs):
     write_metadata(**kwargs)
     return [images, metadata]
 
-# %% ../nbs/00_clip_plot.ipynb 8
+# %% ../nbs/00_clip_plot.ipynb 16
 def get_image_paths(**kwargs):
     """Called once to provide a list of image paths--handles IIIF manifest input"""
     # handle case where --images points to iiif manifest
@@ -299,7 +299,7 @@ def get_image_paths(**kwargs):
         image_paths = image_paths[: kwargs["max_images"]]
     return image_paths
 
-# %% ../nbs/00_clip_plot.ipynb 9
+# %% ../nbs/00_clip_plot.ipynb 17
 def stream_images(**kwargs):
     """Read in all images from args[0], a list of image paths"""
     for idx, i in enumerate(kwargs["image_paths"]):
@@ -320,7 +320,7 @@ def clean_filename(s, **kwargs):
         s = s.replace(i, "")
     return s
 
-# %% ../nbs/00_clip_plot.ipynb 10
+# %% ../nbs/00_clip_plot.ipynb 18
 ##
 # Metadata
 ##
@@ -354,7 +354,7 @@ def get_metadata_list(**kwargs):
             i.update({"tags": i["category"]})
     return l
 
-# %% ../nbs/00_clip_plot.ipynb 11
+# %% ../nbs/00_clip_plot.ipynb 19
 def write_metadata(metadata, **kwargs):
     """Write list `metadata` of objects to disk"""
     if not metadata:
@@ -412,7 +412,7 @@ def write_metadata(metadata, **kwargs):
             **kwargs
         )
 
-# %% ../nbs/00_clip_plot.ipynb 12
+# %% ../nbs/00_clip_plot.ipynb 20
 def is_number(s):
     """Return a boolean indicating if a string is a number"""
     try:
@@ -421,7 +421,7 @@ def is_number(s):
     except:
         return False
 
-# %% ../nbs/00_clip_plot.ipynb 13
+# %% ../nbs/00_clip_plot.ipynb 21
 ##
 # Main
 ##
@@ -504,7 +504,7 @@ def get_manifest(**kwargs):
     }
     write_json(manifest["imagelist"], imagelist, **kwargs)
 
-# %% ../nbs/00_clip_plot.ipynb 14
+# %% ../nbs/00_clip_plot.ipynb 22
 ##
 # Atlases
 ##
@@ -575,7 +575,7 @@ def save_atlas(atlas, out_dir, n):
     out_path = join(out_dir, "atlas-{}.jpg".format(n))
     save_img(out_path, atlas)
 
-# %% ../nbs/00_clip_plot.ipynb 15
+# %% ../nbs/00_clip_plot.ipynb 23
 ##
 # Layouts
 ##
@@ -599,7 +599,7 @@ def get_layouts(**kwargs):
     }
     return layouts
 
-# %% ../nbs/00_clip_plot.ipynb 16
+# %% ../nbs/00_clip_plot.ipynb 24
 def get_inception_vectors(**kwargs):
     """Create and return Inception vector representation of Image() instances"""
     print(
@@ -629,7 +629,7 @@ def get_inception_vectors(**kwargs):
             progress_bar.update(1)
     return np.array(vecs)
 
-# %% ../nbs/00_clip_plot.ipynb 17
+# %% ../nbs/00_clip_plot.ipynb 25
 def get_umap_layout(**kwargs):
     """Get the x,y positions of images passed through a umap projection"""
     vecs = kwargs["vecs"]
@@ -793,7 +793,7 @@ def get_umap_model(**kwargs):
             transform_seed=kwargs["seed"],
         )
 
-# %% ../nbs/00_clip_plot.ipynb 18
+# %% ../nbs/00_clip_plot.ipynb 26
 def get_rasterfairy_layout(**kwargs):
     """Get the x, y position of images passed through a rasterfairy projection"""
     print(timestamp(), "Creating rasterfairy layout")
@@ -910,12 +910,7 @@ def get_custom_layout(**kwargs):
         ),
     }
 
-# %% ../nbs/00_clip_plot.ipynb 19
-##
-# Date Layout
-##
-
-
+# %% ../nbs/00_clip_plot.ipynb 28
 def get_date_layout(cols=3, bin_units="years", **kwargs):
     """
     Get the x,y positions of input images based on their dates
@@ -1049,12 +1044,7 @@ def round_date(date, unit):
             date = str(int(date.split()[-1]) // 100) + "00"
     return date
 
-# %% ../nbs/00_clip_plot.ipynb 20
-##
-# Metadata Layout
-##
-
-
+# %% ../nbs/00_clip_plot.ipynb 30
 def get_categorical_layout(null_category="Other", margin=2, **kwargs):
     """
     Return a numpy array with shape (n_points, 2) with the point
@@ -1190,12 +1180,7 @@ class Box:
         self.x = None if len(args) < 4 else args[3]
         self.y = None if len(args) < 5 else args[4]
 
-# %% ../nbs/00_clip_plot.ipynb 21
-##
-# Geographic Layout
-##
-
-
+# %% ../nbs/00_clip_plot.ipynb 32
 def get_geographic_layout(**kwargs):
     """Return a 2D array of image positions corresponding to lat, lng coordinates"""
     out_path = get_path("layouts", "geographic", **kwargs)
@@ -1238,12 +1223,7 @@ def process_geojson(geojson_path):
         json.dump(l, out)
 
 
-# %% ../nbs/00_clip_plot.ipynb 22
-##
-# Helpers
-##
-
-
+# %% ../nbs/00_clip_plot.ipynb 34
 def get_path(*args, **kwargs):
     """Return the path to a JSON file with conditional gz extension"""
     sub_dir, filename = args
@@ -1395,7 +1375,7 @@ def write_images(**kwargs):
         save_img(out_path, img)
 
 
-# %% ../nbs/00_clip_plot.ipynb 23
+# %% ../nbs/00_clip_plot.ipynb 35
 def get_version():
     """
     Return the version of clipplot installed
@@ -1404,7 +1384,7 @@ def get_version():
     # return pkg_resources.get_distribution("clipplot").version
     return "0.0.1"
 
-# %% ../nbs/00_clip_plot.ipynb 24
+# %% ../nbs/00_clip_plot.ipynb 36
 class Image:
     def __init__(self, *args, **kwargs):
         self.path = args[0]
@@ -1447,12 +1427,7 @@ class Image:
             b[:h, :w, :] = a
         return b
 
-# %% ../nbs/00_clip_plot.ipynb 25
-##
-# Entry Point
-##
-
-
+# %% ../nbs/00_clip_plot.ipynb 38
 def parse(args: Optional[dict] = None):
     """Read command line args and begin data processing"""
     description = "Create the data required to create a clipplot viewer"
@@ -1603,7 +1578,7 @@ def parse(args: Optional[dict] = None):
         config.update(args)
     process_images(**config)
 
-# %% ../nbs/00_clip_plot.ipynb 27
+# %% ../nbs/00_clip_plot.ipynb 40
 args = None
 
 #| hide_line
