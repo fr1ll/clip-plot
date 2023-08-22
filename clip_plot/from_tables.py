64dfe16da3717b1b0cf2e76c45d95c948c7b8052
Will Sanger <willsa@hey.com>
2023-08-22 17:42:14 -0500

    turn meta_columns into list instead of df_meta

--- clip_plot/from_tables.py
+++ clip_plot/from_tables.py
@@ -37,6 +37,6 @@ def table_to_meta(table: pd.DataFrame) -> Tuple[List, List]:
     table = table.rename(columns={"image_filename": "filename"})
     meta_columns = set(table.columns) - set(["image_path", "embed_path"])
     # convert to list as pandas does not let you index with a set
-    df_meta = list(df_meta)
+    meta_columns = list(meta_columns)
     df_meta = table[meta_columns]
     return meta_columns, list(df_meta.to_dict(orient='index').values())
